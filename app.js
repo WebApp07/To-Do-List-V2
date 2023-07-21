7159c158df213a06e3fc72393af597c69477cf23
unknown <AmineZouguig@proton.me>
2023-07-21 03:23:54 -0700

    keep organized

---
+++ app.js
@@ -0,0 +1,59 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+let items = ["Buy food", "Cook food", "Eat food"];
+let workItems = [];
+
+app.set("view engine", "ejs");
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(express.static("public"));
+
+app.get("/", function (req, res) {
+  var today = new Date();
+
+  var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+  };
+
+  var day = today.toLocaleDateString("en-US", options);
+
+  res.render("list", { listTitle: day, newListItems: items });
+});
+
+app.post("/", function (req, res) {
+  let item = req.body.newItem;
+
+  if (req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
+  res.redirect("/");
+});
+
+app.get("/work", function (req, res) {
+  res.render("list", { listTitle: "Work List", newListItems: workItems });
+});
+
+app.post("/work", function (req, res) {
+  workItems.push(item);
+  res.redirect("/work");
+});
+
+app.listen(3000, function () {
+  console.log("Server started on port 3000.");
+});
